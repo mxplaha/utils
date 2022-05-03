4254983676ecd09c83d980b9542a404a51dd76b3
Александр Плахотников <mxplaha@gmail.com>
2022-05-03 06:27:53 +0300

    Change func name ContainsInt to InSliceInt

--- utils.go
+++ utils.go
@@ -3,19 +3,19 @@
 package utils
 
 func InSlice(a []string, x string) bool {
-  for _, n := range a {
-    if x == n {
-      return true
-    }
-  }
-  return false
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
 }
 
-func ContainsInt(a []int, x int) bool {
-  for _, n := range a {
-    if x == n {
-      return true
-    }
-  }
-  return false
+func InSliceInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
 }
